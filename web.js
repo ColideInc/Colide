898195c5aafcd5ab8b6ae43b5a1c3d4f9285e87a
lopesfelipe <felipe.looopes@gmail.com>
2014-03-08 00:57:29 -0300

    usando pg

--- web.js
+++ web.js
@@ -2,6 +2,7 @@
 
 var express = require("express");
 var logfmt = require("logfmt");
+var pg = require( 'pg' );
 var app = express();
 
 app.use( logfmt.requestLogger() );
@@ -13,6 +14,16 @@ app.get( '/' , function( req, res){
 app.get( '/:id' , function( req, res){
 	res.send("Valor do parametro: " + req.params.id);
 });
+
+app.get( '/test/:id' , function( req, res){
+	pg.connect( process.env.DATABASE_URL, function( err, client, done){
+		client.query('SELECT * FROM public."TESTE"', function( err, result){
+			done();
+			if(err) return console.error(err);
+			res.send(result.rows);
+		})
+	})
+})
 
 var port = Number( process.env.PORT || 5000);
 app.listen( port, function(){
